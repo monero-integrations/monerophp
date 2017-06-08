0e0d1f4521ec2b2ebee80b81fa52e99441b67726
serhack <atombt@protonmail.com>
2017-06-08 20:35:34 +0200

    Example updated

--- example.php
+++ example.php
@@ -1,10 +1,17 @@
 <?php
 
 /* Example of Monero Payment  */
-
+require_once('jsonRPCClient.php');
 require_once('Monero_Payments.php');
 
 /* Edit it with your ip and your port of Monero RPC */
 $monero_daemon = new Monero('127.0.0.1','18083');
 
 ?>
+<html>
+  <body>
+    <h1>Example of Monero Library</h1>
+    <h3>Monero Address</h3>
+    <?php $monero_daemon->address(); ?>
+  </body>
+</html>
