223248c3f0d67683b91ef40b14e6a9a4ae3beb11
serhack <atombt@protonmail.com>
2017-06-08 20:16:23 +0200

    Fixed a typo

--- example.php
+++ example.php
@@ -2,7 +2,7 @@
 
 /* Example of Monero Payment  */
 
-require_once('Monero.php');
+require_once('Monero_Payments.php');
 
 /* Edit it with your ip and your port of Monero RPC */
 $monero_daemon = new Monero('127.0.0.1','18083');
