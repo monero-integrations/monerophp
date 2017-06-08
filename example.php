8a59c11414550610f279e35746b818ede2eb17e7
serhack <atombt@protonmail.com>
2017-06-08 20:15:58 +0200

    Example.php

---
+++ example.php
@@ -0,0 +1,10 @@
+<?php
+
+/* Example of Monero Payment  */
+
+require_once('Monero.php');
+
+/* Edit it with your ip and your port of Monero RPC */
+$monero_daemon = new Monero('127.0.0.1','18083');
+
+?>
