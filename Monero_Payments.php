22f525eb93a20b35c2a52485f9aa7120950d9c5d
serhack <atombt@protonmail.com>
2017-06-18 14:37:18 +0200

    Update Monero_Payments.php

--- Monero_Payments.php
+++ Monero_Payments.php
@@ -71,19 +71,47 @@
          $this->_print($height);
   }
   
-  /*
-   * Incoming Transfer
-   * $type must be All 
-   */
-  public function incoming_transfer($type){
-   $incoming_parameters = array('transfer_type' => $type);
-   $incoming_transfer = $this->_run('incoming_transfer', $incoming_parameters);
-   $this->_print($incoming_transfer);
-  }
+     /*
+     * Incoming Transfer
+     * $type must be All 
+     */
+   public function incoming_transfer($type){
+        $incoming_parameters = array('transfer_type' => $type);
+        $incoming_transfer = $this->_run('incoming_transfer', $incoming_parameters);
+        $this->_print($incoming_transfer);
+    }
   
-  public function get_transfers(){
-   $get_parameters = array('pool' => true);
-   $get_transfers = $this->_run('get_transfers', $get_parameters);
-   $this->_print($get_transfers);
-  }
+    public function get_transfers(){
+        $get_parameters = array('pool' => true);
+        $get_transfers = $this->_run('get_transfers', $get_parameters);
+        $this->_print($get_transfers);
+    }
+     
+     public function view_key(){
+    $query_key = array('key_type' => 'view_key');
+    $query_key_method = $this->_run('query_key', $query_key);
+    $this->_print($query_key_method);
+     }
+     
+     public function make_integrated_address($payment_id){
+         if(isset($payment_id)){
+             $integrate_address_parameters = array('payment_id' => '');
+         }
+         else{
+             $integrate_address_parameters = array('payment_id' => $payment_id);
+         }
+        $integrate_address_method = $this->_run('make_integrated_address', $integrate_address_parameters);
+        $this->_print($integrate_address_method);
+     }
+     
+    public function split_integrated_address($integrated_address){
+        if(isset($integrated_address)){
+            echo "Error: Integrated_Address mustn't be null";
+        }
+        else{
+        $split_params = array('integrated_address' => $integrated_address);
+        $split_methods = $this->_run('split_integrated_address', $split_params);
+        $this->_print($split_methods);
+        }
+    }
  
