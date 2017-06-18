691e7379f99623d9d708632345c3075599faa5f0
serhack <atombt@protonmail.com>
2017-06-18 21:54:12 +0200

    Update example.php

--- example.php
+++ example.php
@@ -13,5 +13,7 @@ $monero_daemon = new Monero('127.0.0.1','18083');
     <h1>Example of Monero Library</h1>
     <h3>Monero Address</h3>
     <?php $monero_daemon->address(); ?>
+    <h3>Balance</h3>
+    <?php $monero_daemon->getbalance(); ?>
   </body>
 </html>
