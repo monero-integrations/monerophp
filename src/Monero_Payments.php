006b7be76e05aa80d57d608c11dfd70aa276c1fa
serhack <atombt@protonmail.com>
2017-08-23 20:11:11 +0200

    Update Monero_Payments.php

--- src/Monero_Payments.php
+++ src/Monero_Payments.php
@@ -21,7 +21,7 @@
      *  @param  $ip   IP of Monero RPC
      *  @param  $port Port of Monero RPC
      */
-    function __construct ($ip = '127.0.0.1', $port, $protocol = 'http', $user, $password){
+    function __construct ($ip = '127.0.0.1', $port, $protocol = 'http', $user = null, $password = null){
         $this->ip = $ip;
         $this->port = $port;
         // I need to implement a sort of validating http or https
