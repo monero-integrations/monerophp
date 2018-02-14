365bc2277cb1de12fd9d2a97e185bd511cf79139
cryptochangements34 <bevanoffr@gmail.com>
2018-02-13 19:48:24 -0600

    Add sc_reduce function
    Implements sc_reduce as described in MRL-0003 4.1.12

--- src/cryptonote.php
+++ src/cryptonote.php
@@ -1,9 +1,17 @@
 <?php
-    require_once "SHA3.php";
+/*
+Copyright (c) 2018 Monero-Integrations
+*/
+    require_once("SHA3.php");
+    require_once("ed25519.php");
     
     class Cryptonote
-    { 
-        
+    {
+        protected $ed25519;
+        public function __construct()
+        {
+            $this->ed25519 = new ed25519();
+        }
         public function keccak_256($message)
         {
             $keccak256 = SHA3::init (SHA3::KECCAK_256);
@@ -13,10 +21,18 @@
         
         public function gen_new_hex_seed()
         {
-            $bytes = random_bytes(64);
+            $bytes = random_bytes(32);
             return bin2hex($bytes);
         }
         
-        
+        public function sc_reduce($input)
+        {
+            $integer = $this->ed25519->decodeint(hex2bin($input));
+            
+            $modulo = bcmod($integer , $this->ed25519->l);
+            
+            $result = bin2hex($this->ed25519->encodeint($modulo));
+            return $result;
+        }
         
     }
