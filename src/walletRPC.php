9b142372a0fb74ba6e58bb4481f10b67a13986a1
sneurlax <sneurlax@gmail.com>
2018-04-01 23:19:34 -0700

    Closes #59

--- src/walletRPC.php
+++ src/walletRPC.php
@@ -1278,7 +1278,7 @@ class walletRPC
   public function get_transfers($input_types = ['all'], $account_index = 0, $subaddr_indices = '', $min_height = 0, $max_height = 4206931337)
   {
     if (is_string($input_types)) { // If user is using old method
-      $get_transfers_parameters = array($input_type => $account_index); // $get_transfers_parameters = array($input_type => $input_value);
+      $get_transfers_parameters = array($input_types => $account_index); // $get_transfers_parameters = array($input_type => $input_value);
     } else {
       if (is_object($input_types)) { // Parameters passed in as object/dictionary
         $params = $input_types;
