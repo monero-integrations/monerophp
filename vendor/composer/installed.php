76138105cb1372e3974696f07c6815aa204b45e1
Fabio Tea <f4bio@users.noreply.github.com>
2023-03-27 14:09:07 +0000

    removed gitignore; fetched latest changes

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.1',
+    'version' => '1.0.1.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'monero-integrations/monerophp',
+  ),
+  'versions' => 
+  array (
+    'monero-integrations/monerophp' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
